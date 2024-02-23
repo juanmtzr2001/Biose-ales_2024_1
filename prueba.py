0bf28c5557187099d3b46cfd71c03754f47a5532
Juan Martinez <juan.martinezr2@udea.edu.co>
2024-02-22 22:39:05 -0500

    Cambios en el taller

--- prueba.py
+++ prueba.py
@@ -1,1 +1,10 @@
-print("Hola mundo")
+import pandas as pd
+import numpy as np
+import matplotlib as mp
+
+vector_a = np.array([3.1, 1, -0.5, -3.2, 6])
+#vector_a = np.array([3.1, 1, -0.5, -3.2, 6])
+vector_b = np.array([1, 3, 2.2, 5.1, 1])
+
+product_sc = vector_a @ vector_b
+prduct_mtr = vector_a * vector_b
